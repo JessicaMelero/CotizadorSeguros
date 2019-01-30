e131cf27b6dd97cfe5f0075a25251923d9c11fde
Jessica Melero <jessicameleromartinez@MacBook-Pro-de-Jessica-4.local>
2019-01-30 16:15:52 +0100

    Creado el resultado de la cotizacion segun diferencias y un archivo helper.js con funciones globales

--- src/componentes/App.js
+++ src/componentes/App.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import Header from './Header';
 import Formulario from './Formulario';
+import {obtenerDiferenciaAnio, calcularMarca, obtenerPlan} from '../helper';
 
 class App extends Component {
 
   cotizarSeguro = (datos) => {
-    console.log(datos);
+    const {marca, plan, year} = datos;
+
+    // Agregar una base de 2000
+    let resultado = 2000;
+
+    // Obtener la diferencia de años
+    const diferencia = obtenerDiferenciaAnio(year);
+
+    // Por cada uno menos restar 3% al valor del seguro
+    resultado -= ((diferencia*3) * resultado)/100;
+
+    // Americano 15%, Asiatico 5%, Europeo 30% de incremento del valor actual
+    resultado = calcularMarca(marca) * resultado;
+
+    // plan del auto, basico incrementa 20% / completa incrementa 50%
+    let incrementaPlan = obtenerPlan(plan);
+    
+    // dependiendo del plan incrementar
+    resultado = parseFloat(incrementaPlan * resultado).toFixed(2);
+
+    //ya tenemos el coste
+
+    console.log(resultado);
   }
 
   render() {
